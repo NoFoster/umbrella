da6e683ad6a8394d0cb2e7302cbf9c90a83ec4ff
NoFoster <kfoster0@chicagobooth.edu>
2025-04-08 03:32:11 +0000

    Pulling hourly rain data

--- umbrella.rb
+++ umbrella.rb
@@ -10,7 +10,7 @@ pp "Hello where are you?"
 
 ##my_location = gets.chomp
 my_location = "Chicago Booth Harper Center"
-
+##pp "Checking the weather in #{my_location}...."
 google_maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{my_location}&key=#{gmaps_key}"
 raw_body = HTTP.get(google_maps_url).to_s
 require "json"
@@ -23,11 +23,27 @@ location_hash = geometry_hash.fetch("location")
 lat = location_hash.fetch("lat")
 lng = location_hash.fetch("lng")
 
- pirate_weather_url = "https://api.pirateweather.net/forecast/#{pw_api_key}/#{lat.round(4)},#{lng.round(4)}"
+ pirate_weather_url = "https://api.pirateweather.net/forecast/#{pw_api_key}/#{lat},#{lng}"
 raw_response = HTTP.get(pirate_weather_url)
-pp parsed_response = JSON.parse(raw_response)
-
+parsed_response = JSON.parse(raw_response)
+hourly_data_array = parsed_response.fetch("hourly").fetch("data")
 currently_hash = parsed_response.fetch("currently")
-
 current_temp = currently_hash.fetch("temperature")
-puts current_temp
+
+##pp "Your coordinates are #{lat},#{lng}."
+##pp "The current temperature is #{current_temp}."
+
+
+def extract_time_and_precip(hourly_data_array)
+  time_precip_hash = {}
+  hourly_data_array.each do |hour_hash|
+    time_stamp = hour_hash.fetch("time")
+    precip_probability = hour_hash.fetch("precipProbability")
+    time_precip_hash.store(time_stamp, precip_probability)
+  end
+  return time_precip_hash
+end
+
+
+result_hash = extract_time_and_precip(hourly_data_array)
+pp result_hash
